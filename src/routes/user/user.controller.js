c9ae015cc3edec3c284be3b93f6e7b3de3a0e496
sunnysakib <sakibur.rahaman033@gmail.com>
2024-08-13 00:35:09 +0600

    fix: fix user creation

--- src/routes/user/user.controller.js
+++ src/routes/user/user.controller.js
@@ -2,8 +2,12 @@ const bcrypt = require('bcryptjs');
 const User = require('../../models/user.model');
 
 const userRegister = async (req, res) => {
-  const { name, email, password, role } = req.body;
-
+  const { name, email, password } = req.body;
+  const user = {
+    name,
+    email,
+    role: 'user',
+  }
   try {
     const [existingUser] = await User.findByEmail(email);
     if (existingUser.length > 0) {
@@ -11,9 +15,9 @@ const userRegister = async (req, res) => {
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    await User.create(name, email, hashedPassword, role);
+    await User.create(name, email, hashedPassword, user.role);
 
-    res.status(201).json({ message: 'User registered successfully' });
+    res.status(201).json({ message: 'User registered successfully', data: user });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
